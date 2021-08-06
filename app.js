e96a439e9869240a4f2ec55c911d769a201b978d
chinmay <chinmay11.moghe@gmail.com>
2021-08-06 21:11:15 +0530

    fix unobserve code

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const targets = document.querySelectorAll('.grid-item');
 if (window.IntersectionObserver) {
   const observercb = (entries) => {
     entries.forEach(entry => {
-      console.log(entry, entry.intersectionRatio);
       if (entry.isIntersecting) {
         entry.target.classList.add('appear');
-        entry.unobserve();
+        intersectObserve.unobserve(entry.target);
       }
     });
-  }
+  };
 
   const intersectObserve = new IntersectionObserver(observercb);
   targets.forEach(target => intersectObserve.observe(target));
